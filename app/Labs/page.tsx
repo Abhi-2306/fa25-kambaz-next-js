e38ef7b9f83d15b884a68af3a93882bc1575b1d6
Abhi-2306 <abhijtih.pandiri@gmail.com>
2025-09-22 15:30:44 -0400

    added Labs/page.tsx

---
+++ app/Labs/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Labs() {
+    return (
+        <div>
+            <h1>Abhijith Pandiri - Section 04</h1>
+            <h2>Source code repositories</h2>
+            <ul>
+                <li><a id="wd-github" href="https://github.com/Abhi-2306/fa25-kambaz-next-js">Github Repository - fa25-kambaz-next-js</a></li>
+            </ul>
+        </div>
+    );
+}
