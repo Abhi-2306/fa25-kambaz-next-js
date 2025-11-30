402e2b07592e18ed39b860266f9964b8a5599663
Abhi-2306 <abhijtih.pandiri@gmail.com>
2025-11-30 14:56:48 -0500

    completed 2

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -14,7 +14,7 @@ export default function CourseNavigation() {
             {links.map((link) => (
                 <Link
                     key={link}
-                    href={`/Courses/${cid}/${link === "People" ? "People/Table" : link}`}
+                    href={`/Courses/${cid}/${link === "People" ? "People" : link}`}
                     id={`wd-course-${link.toLowerCase()}-link`}
                     className={`list-group-item border-0
                         ${pathname.includes(`/${link}`) ? "active" : "text-danger"}`}
