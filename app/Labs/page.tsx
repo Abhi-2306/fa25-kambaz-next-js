b4ed0d9f13304cbd4c8ed0ce3fc24fce9174626c
Sidhartha Yadav Vadlakonda <sidharthayadav@Sidharthas-MacBook-Pro.local>
2025-12-08 12:13:57 -0500

    labs updated

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -1,12 +1,13 @@
-
 export default function Labs() {
     return (
         <div>
             <h1>Abhijith Pandiri - Section 04</h1>
+            <h1>Sidhartha Yadav Vadlakonda - Section 04</h1>
+            <h1>Tanishq Neela - Section 04</h1>
             <h2>Source code repositories</h2>
             <ul>
-                <li><a id="wd-github" href="https://github.com/Abhi-2306/fa25-kambaz-next-js">Github Repository - fa25-kambaz-next-js</a></li>
-                <li><a id="wd-github-backend" href="https://github.com/Abhi-2306/kambaz-node-server-app">Github Repository - kambaz-node-server-app</a></li>
+                <li><a id="wd-github" href="https://github.com/Abhi-2306/fa25-kambaz-next-js/tree/project">Github Repository - fa25-kambaz-next-js</a></li>
+                <li><a id="wd-github-backend" href="https://github.com/Abhi-2306/kambaz-node-server-app/tree/project">Github Repository - kambaz-node-server-app</a></li>
             </ul>
         </div>
     );
